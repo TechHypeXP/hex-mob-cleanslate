80b1ea4bf66fcd2a6da136690a58a7f32162c116
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:31:39 +0300

    feat(structure): move SwipeCard to correct UI location

---
+++ apps/mobile/src/ui/components/SwipeCard.tsx
@@ -0,0 +1,12 @@
+// Placeholder for SwipeCard - will be implemented according to UI layer principles
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const SwipeCard = () => {
+  return (
+    <View>
+      <Text>Swipe Card</Text>
+      {/* TODO: Implement swipe card according to UI layer rules */}
+    </View>
+  );
+};
