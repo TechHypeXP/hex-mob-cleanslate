362ae15ec660d1d8f0cda2a281ab96a2e8a95d0a
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:31:22 +0300

    feat(structure): move TabNavigation to correct UI location

---
+++ apps/mobile/src/ui/navigation/TabNavigation.tsx
@@ -0,0 +1,12 @@
+// Placeholder for TabNavigation - will be implemented according to UI layer principles
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const TabNavigation = () => {
+  return (
+    <View>
+      <Text>Tab Navigation</Text>
+      {/* TODO: Implement tab navigation according to UI layer rules */}
+    </View>
+  );
+};
