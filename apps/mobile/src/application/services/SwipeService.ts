76ba6879358609ef480233f7b45b1b7ea762ccd1
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:30:12 +0300

    feat(structure): move SwipeService to correct application location

---
+++ apps/mobile/src/application/services/SwipeService.ts
@@ -0,0 +1,4 @@
+// Placeholder for SwipeService - will be implemented according to application layer principles
+export class SwipeService {
+  // TODO: Implement swipe service according to application layer rules
+}
