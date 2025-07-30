883276aaf590dd3d8baccd0eaae079c977167fbb
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:32:08 +0300

    feat(structure): move SpinWheel to correct UI location

---
+++ apps/mobile/src/ui/components/SpinWheel.tsx
@@ -0,0 +1,12 @@
+// Placeholder for SpinWheel - will be implemented according to UI layer principles
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const SpinWheel = () => {
+  return (
+    <View>
+      <Text>Spin Wheel</Text>
+      {/* TODO: Implement spin wheel according to UI layer rules */}
+    </View>
+  );
+};
