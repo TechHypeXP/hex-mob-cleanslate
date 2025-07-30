0f4cfd60de2e835054eb12eb93a7c1725f6d9400
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:29:34 +0300

    feat(structure): move GamificationEntity to correct domain location

---
+++ apps/mobile/src/domain/entities/GamificationEntity.ts
@@ -0,0 +1,4 @@
+// Placeholder for GamificationEntity - will be implemented according to DDD principles
+export class GamificationEntity {
+  // TODO: Implement gamification entity according to domain rules
+}
