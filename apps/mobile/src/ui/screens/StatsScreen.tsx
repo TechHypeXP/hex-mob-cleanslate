a4091fd33f2c76acd3959f594f5cacb4beaaa59a
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:31:03 +0300

    feat(structure): move StatsScreen to correct UI location

---
+++ apps/mobile/src/ui/screens/StatsScreen.tsx
@@ -0,0 +1,12 @@
+// Placeholder for StatsScreen - will be implemented according to UI layer principles
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const StatsScreen = () => {
+  return (
+    <View>
+      <Text>Stats Screen</Text>
+      {/* TODO: Implement stats screen according to UI layer rules */}
+    </View>
+  );
+};
