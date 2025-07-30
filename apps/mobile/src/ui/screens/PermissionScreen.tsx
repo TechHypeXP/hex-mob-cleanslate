a7fe867e31d2549c8b6b59608339ae813cf40c04
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2025-07-31 00:30:33 +0300

    feat(structure): move PermissionScreen to correct UI location

---
+++ apps/mobile/src/ui/screens/PermissionScreen.tsx
@@ -0,0 +1,12 @@
+// Placeholder for PermissionScreen - will be implemented according to UI layer principles
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const PermissionScreen = () => {
+  return (
+    <View>
+      <Text>Permission Screen</Text>
+      {/* TODO: Implement permission screen according to UI layer rules */}
+    </View>
+  );
+};
